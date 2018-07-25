c977bef7e131ad22215de8817c078888418fbc7f
titodasilva <41694142+titodasilva@users.noreply.github.com>
2018-07-25 13:03:21 -0300

    corrigindo um vacilo

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
-# ftp@Anim4l1404
-rsync -avz --exclude 'LICENSE' --exclude 'README.md' * anilab1@ftp.anilab.net:~/public_html
+cat "Use a senha 'ftp@Anim4l1404'"
+rsync -avz --exclude 'LICENSE' --exclude 'README.md' --exclude 'sync.sh' --exclude 'passo-a-passo.txt' * anilab1@ftp.anilab.net:~/public_html
 
